01ac187b8a184c10fd43890a6895bd231c25265f
Lv Zhaobo <lvzhaobo@gmail.com>
2014-10-17 13:43:18 +0800

    内容完善整理，正式版发布

--- account.php
+++ account.php
@@ -1,4 +1,4 @@
 <?php
 	$fh = fopen("account.txt","a+");
-	fwrite($fh,"UA:".$_GET['ua']."	"."IP:".$_GET['ip']."	"."URL:".$_GET['url']."\r\n");
+	fwrite($fh,date("Y-m-d H:i:s",time()). "	IP:".$_SERVER['REMOTE_ADDR']."	URL:".$_GET['url']."	UA:".$_GET['ua']."\r\n");
 	fclose($fh);
